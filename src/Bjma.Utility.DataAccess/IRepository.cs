b5f82476c7cce88cee08d0c10b1a85d476fff21e
Ben Aston <ben.aston@justgiving.com>
2011-10-23 04:49:52 +0100

    Ids now strings.

--- src/Bjma.Utility.DataAccess/IRepository.cs
+++ src/Bjma.Utility.DataAccess/IRepository.cs
@@ -3,11 +3,14 @@
     using System;
     using System.Collections.Generic;
 
-    public interface IRepository<T> //  where T : new()
+    public interface IRepository<T>
     {
-        T GetById(Guid id);
-        IEnumerable<T> GetByIds(IEnumerable<Guid> ids);
-        IEnumerable<T> GetByFreeText(string query, int startPage, int numberOfPages);
+        T GetById(string id);
+
+        IEnumerable<T> GetByIds(IEnumerable<string> ids);
+
+        IEnumerable<T> GetByFreeText(string query, int pageSize, int startPage, int numberOfPages);
+
         Guid Save(T objectToSave);
     }
 }
